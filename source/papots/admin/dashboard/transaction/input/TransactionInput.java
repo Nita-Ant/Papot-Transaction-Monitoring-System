9aaf09b81f303841a09142c486e11333f6d83e52
elcuenca <elmercuenca74@gmail.com>
2021-07-15 09:11:54 +0800

    Created a transaction input UI skeleton

---
+++ source/papots/admin/dashboard/transaction/input/TransactionInput.java
@@ -0,0 +1,14 @@
+package papots.admin.dashboard.transaction.input;
+
+import javax.swing.JPanel;
+
+public class TransactionInput extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public TransactionInput() {
+
+	}
+
+}
