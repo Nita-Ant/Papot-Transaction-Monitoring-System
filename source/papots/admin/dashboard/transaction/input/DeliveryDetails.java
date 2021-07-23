d9c0bd9c979395b665a5bd6d25d7fbf969441a0d
Nita <nitaantasuda@gmail.com>
2021-07-24 03:53:00 +0800

    [WORKING] Also connects to database

--- source/papots/admin/dashboard/transaction/input/DeliveryDetails.java
+++ source/papots/admin/dashboard/transaction/input/DeliveryDetails.java
@@ -8,10 +8,20 @@ import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+
 import com.toedter.calendar.JDateChooser;
 import javax.swing.JTextField;
 
@@ -20,9 +30,12 @@ public class DeliveryDetails extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 	private JTextField txtMOD;
 	private JTextField txtShipFee;
+	protected TransactionInput objInput;
+	protected OrderDetails objOrder;
+	Connection objCon;
 
 	
-	public DeliveryDetails() {
+	public DeliveryDetails() throws ClassNotFoundException {
 		setBounds(100, 100, 452, 326);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -37,7 +50,7 @@ public class DeliveryDetails extends JDialog {
 		JLabel lblShipfee = new JLabel("Shipping Fee:");
 		lblShipfee.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		
-		JDateChooser CalReceived = new JDateChooser();
+		JDateChooser calReceived = new JDateChooser();
 		
 		txtMOD = new JTextField();
 		txtMOD.setColumns(10);
@@ -58,13 +71,12 @@ public class DeliveryDetails extends JDialog {
 						.addComponent(lblMOD))
 					.addPreferredGap(ComponentPlacement.UNRELATED)
 					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING, false)
-							.addComponent(txtMOD)
-							.addComponent(CalReceived, GroupLayout.DEFAULT_SIZE, 173, Short.MAX_VALUE))
+						.addComponent(txtMOD)
+						.addComponent(calReceived, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE)
 						.addComponent(txtShipFee, GroupLayout.PREFERRED_SIZE, 173, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap(24, Short.MAX_VALUE))
 				.addGroup(gl_contentPanel.createSequentialGroup()
-					.addContainerGap(369, Short.MAX_VALUE)
+					.addContainerGap(357, Short.MAX_VALUE)
 					.addComponent(btnOK)
 					.addContainerGap())
 		);
@@ -73,25 +85,103 @@ public class DeliveryDetails extends JDialog {
 				.addGroup(gl_contentPanel.createSequentialGroup()
 					.addGap(48)
 					.addGroup(gl_contentPanel.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(CalReceived, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(calReceived, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 						.addComponent(lblMOD, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 					.addGap(29)
-					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(txtMOD, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblReceived, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(lblReceived)
+						.addComponent(txtMOD, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))
 					.addGap(36)
 					.addGroup(gl_contentPanel.createParallelGroup(Alignment.LEADING)
 						.addComponent(lblShipfee)
 						.addComponent(txtShipFee, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.RELATED, 37, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
 					.addComponent(btnOK)
 					.addContainerGap())
 		);
 		contentPanel.setLayout(gl_contentPanel);
 		
+		//CREATE CONNECTION
+		createconn();
+				
+		
+		//add action listener to btnOK
+		btnOK.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+				try {
+					int OrderStatus = 1;
+					
+					//format Date
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+					String date = sdf.format(calReceived.getDate());
+					
+					/*
+					//CHECKING OF VALIDATION OF DATES
+					java.util.Date start = new SimpleDateFormat("yyyy/MM/dd").parse(date);
+		            java.util.Date end = new SimpleDateFormat("yyyy/MM/dd").parse(objOrder.getDate());
+		            
+		            if (start.compareTo(end) < 0) {
+		                JOptionPane.showMessageDialog(null, "Invalid Date. Please try again");
+		            } 
+					*/
+					
+					
+					//GET THE SHIP_FEE
+					float ShipFee = Float.parseFloat(txtShipFee.getText());
+					
+					//GET MOD
+					String MOD = txtMOD.getText();
+					
+					//GET GRAND TOTAL
+					float grand_total = objInput.getGrand_total() + ShipFee;
+					
+					//ADD TO THE DATABASE		
+					//Create a Statement object that will allow us to do operation
+					Statement objstmt = objCon.createStatement();
+			                
+			        //Create the statement that will manipulate data
+			        String strOp = "INSERT INTO transaction(transaction_no,order_date, supplier_id, mode_of_delivery,order_status,date_received,subtotal,ship_fee,grand_total) VALUES ('"+objInput.getTransaction()+"','"+ objOrder.getDate()
+			        +"','"+objInput.getSupplier()+"', '"+MOD+"','"+OrderStatus+"', '"+date+"','"+objInput.getGrand_total()+"', '"+ShipFee+"', '"+grand_total+"')";
+			              
+			        //insert into the database
+			        objstmt.execute(strOp);
+			        objstmt.close();
+			        
+			        JOptionPane.showMessageDialog(null,"Successfully Added");
+			        calReceived.setDate(null);
+			        txtShipFee.setText("");
+			        txtMOD.setText("");
+			        dispose();
+			        objInput.setGrand_total(0);
+
+						
+					
+				}catch (Exception ex) {
+					JOptionPane.showMessageDialog(null,ex);
+				}
+				
+			}
+		});
+		
+		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		this.setVisible(true);
 	
+	}
+	void createconn() throws ClassNotFoundException{
+	    try {
+	        
+	            //load the driver
+	           Class.forName("com.mysql.cj.jdbc.Driver");
+	           
+	           //connect to the database
+	           objCon = DriverManager.getConnection("jdbc:mysql://localhost:3306/papot-db", "root", "haycab99");
+	           
+	           
+	    }catch(SQLException ex) {
+	    	JOptionPane.showMessageDialog(null,ex);
+	    }
 	
 	}
 }
